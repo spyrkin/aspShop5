4ddb570182681b3232a9ca986233e5d476082727
Sergey Pyrkin <sergey.pyrkin@instatsport.com>
2023-06-21 22:55:57 +0400

    add first pages

--- ShopAspNet5/Startup.cs
+++ ShopAspNet5/Startup.cs
@@ -16,6 +16,7 @@ namespace ShopAspNet5
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,9 +26,14 @@ namespace ShopAspNet5
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
             app.UseRouting();
-
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}");
+            });
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
